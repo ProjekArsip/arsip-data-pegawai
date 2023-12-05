b1945e476ac1cc1745df65a9e8d4273a361d12fb
ProjekArsip <142193451+ProjekArsip@users.noreply.github.com>
2023-12-05 10:46:26 +0800

    insert.blade.php
    insert udah ku upate, tinggal buat databasenya, terus kalau bisa habis di input dari insert masukan ke file lihat.blade.php, biar habis di input langsung muncul ke tampilkan data

--- resources/views/pegawai/insert.blade.php
+++ resources/views/pegawai/insert.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ARSIP DATA PEGAWAI</title>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"
         integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 <head>
@@ -13,9 +14,9 @@
     <title>Form Input Data Pegawai</title>
     <style>
         body {
-            margin-top:40% ; 
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
+            margin-top:40% ;
+            font-family: poppins;
+            background-color: #F3EFE0;
             margin: 20px;
         }
 
@@ -36,7 +37,7 @@
             display: inline-block;
             width: 150px;
             margin-bottom: 8px;
-            position:; 
+            position:;
         }
 
         input, select {
@@ -47,13 +48,15 @@
         }
 
         input[type="submit"] {
-            background-color: #4caf50;
+            background-color: #22A39F;
             color: #fff;
             cursor: pointer;
+            border-color: #22A39F;
+            border-radius: 5px;
         }
 
         input[type="submit"]:hover {
-            background-color: #45a049;
+            background-color: #1f7875;
         }
         form {
     font-size: 18px; /* Ubah ukuran font formulir */
@@ -71,6 +74,9 @@ input, select {
 input[type="submit"] {
     font-size: 18px; /* Ubah ukuran font tombol submit */
 }
+input[type='file'] {
+    font-size: 18px;
+}
 
 
 
@@ -81,12 +87,12 @@ input[type="submit"] {
 
 
 
-
-        body {
-            background-image: linear-gradient(to right, #EDF1D6, #40513B, #40513B, #EDF1D6);
+    body {
+            background-color: #434242;
             color: #000;
             margin: 0;
             padding: 0;
+            font-family: poppins;
         }
 
         .display-table {
@@ -107,19 +113,9 @@ input[type="submit"] {
         }
 
         #navigation {
-    width: 250px;
-    background-image: linear-gradient(to right, #40513B,  #EDF1D6);
-    height: 100vh; /* Ganti properti height menjadi 100vh */
-    position: fixed;
-}
-
-.display-table-cell {
-    display: table-cell;
-    float: none;
-    vertical-align: top;
-    position: relative;
-}
-
+            background-color: #F3EFE0;
+            width: 250px;
+        }
 
         .logo {
             text-align: center;
@@ -127,7 +123,6 @@ input[type="submit"] {
         }
 
         .logo h3 {
-            color: white;
             margin: 0;
         }
 
@@ -145,9 +140,9 @@ input[type="submit"] {
         }
 
         .navi a {
-            color: white;
+            color: black;
             text-decoration: none;
-            font-size: 16px;
+            font-size: 20px;
             transition: color 0.3s; /* Efek transisi warna */
         }
 
@@ -160,92 +155,97 @@ input[type="submit"] {
         }
 
         .navi .active a {
-            color: white /* Warna hitam saat aktif */
+            color: black /* Warna hitam saat aktif */
         }
 
         .content {
             padding: 20px;
         }
 
-
-
-        
     </style>
 </head>
 
 <body>
-    <div class="display-table">
+   <div class="display-table">
         <div class="display-table-row">
             <div class="display-table-cell" id="navigation">
                 <div class="logo">
-                    <h3>ARSIP DATA PEGAWAI</h3>
+                    <h3>ARSIP DATA <br> PEGAWAI</h3>
                 </div>
                 <div class="navi">
                     <ul>
-                        <li class=""><a href="dashboard"><i class="fa fa-home" aria-hidden="true"></i><span>Home</span></a>
+                        <li class=""><a href="dashboard"><i class="fa fa-home" aria-hidden="true"></i><span> Beranda</span></a>
                         </li>
-                        <li class="active"><a href="insert"><i class="fa fa-tasks" aria-hidden="true"></i><span>Tambah Data</span></a></li>
-                        <li><a href="#"><i class="fa fa-bar-chart" aria-hidden="true"></i><span>Tampilkan Data</span></a>
+                        <li class="active"><a href="insert"><i class="fa fa-tasks" aria-hidden="true"></i><span> Tambah Data</span></a></li>
+                        <li><a href="lihat"><i class="fa fa-bar-chart" aria-hidden="true"></i><span> Tampilkan Data</span></a>
                         </li>
-                        <li><a href="login"><i class="fa fa-cog" aria-hidden="true"></i><span>Log Out</span></a></li>
+                        <li><a href="login"><i class="fa fa-sign-out" aria-hidden="true"></i><span> Keluar</span></a></li>
                     </ul>
                 </div>
-            </div>          
+            </div>
             <br><br>
+
             <div class="card">
                 <form action="/tambah/simpan" method="post">
-                    @csrf 
-                    <label for="nip">NIP:</label>
+                    @csrf
+                    <label for="nip">NIP :</label>
                     <input type="text" id="nip" name="nip" required>
-            
-                    <label for="nama">Nama:</label>
-                    <input type="text" id="nama" name="nama" required>
-                    
-                    <label for="jenis_kelamin">Jenis Kelamin:</label>
+
+                    <label for="nama">Nama :</label>
+                    <input type="text" id="nama_pegawai" name="nama" required>
+
+                    <label for="jenis_kelamin">Jenis Kelamin :</label>
                     <select id="jenis_kelamin" name="jenis_kelamin" required>
-                        <option value="Menikah">Laki-laki</option>
-                        <option value="Belum Menikah">Perempuan</option>
+                        <option value="Laki-Laki">Laki-laki</option>
+                        <option value="Perempuan">Perempuan</option>
                     </select>
-            
-                    <label for="tempat_lahir">Tempat Lahir:</label>
+
+                    <label for="tempat_lahir">Tempat Lahir :</label>
                     <input type="text" id="tempat_lahir" name="tempat_lahir" required>
-            
-                    <label for="tanggal_lahir">Tanggal Lahir:</label>
+
+                    <label for="tanggal_lahir">Tanggal Lahir :</label>
                     <input type="date" id="tanggal_lahir" name="tanggal_lahir" required>
-            
-                    <label for="status">Status:</label>
+
+                    <label for="status">Status :</label>
                     <select id="status" name="status" required>
                         <option value="Menikah">Menikah</option>
                         <option value="Belum Menikah">Belum Menikah</option>
                     </select>
-            
-                    <label for="golongan">Golongan:</label>
+
+                    <label for="golongan">Golongan :</label>
                     <input type="text" id="golongan" name="golongan" required>
-            
-                    <label for="tamat_golongan">Tamat Golongan:</label>
+
+                    <label for="tamat_golongan">Tamat Golongan :</label>
                     <input type="date" id="tamat_golongan" name="tamat_golongan" required>
-            
-                    <label for="pendidikan_terakhir">Pendidikan Terakhir:</label>
+
+                    <label for="pendidikan_terakhir">Pendidikan Terakhir :</label>
                     <input type="text" id="pendidikan_terakhir" name="pendidikan_terakhir" required>
-            
-                    <label for="jenis_jabatan">Jenis Jabatan:</label>
+
+                    <label for="jenis_jabatan">Jenis Jabatan :</label>
                     <input type="text" id="jenis_jabatan" name="jenis_jabatan" required>
-            
-                    <label for="tanggal_mulai_tugas_pns">Tanggal Mulai Tugas PNS:</label>
-                    <input type="date" id="tanggal_mulai_tugas_pns" name="tanggal_mulai_tugas_pns" required>
-            
-                    <label for="tanggal_mulai_tugas_golongan">Tanggal Mulai Tugas Golongan:</label>
-                    <input type="date" id="tanggal_mulai_tugas_golongan" name="tanggal_mulai_tugas_golongan" required>
-            
-                    <label for="bidang">Bidang:</label>
+
+                    <label for="tanggal_mulai_tugas_pns">Tanggal Mulai <br> Tugas PNS :</label>
+                    <input type="date" id="tmt_pns" name="tanggal_mulai_tugas_pns" required>
+
+                    <label for="tanggal_mulai_tugas_golongan">Tanggal Mulai <br> Tugas Golongan :</label>
+                    <input type="date" id="tmt_golongan" name="tanggal_mulai_tugas_golongan" required>
+
+
+                    <label for="bidang">Bidang : </label>
                     <input type="text" id="bidang" name="bidang" required>
 
-                    <label for="file">Pilih File:</label>
-                    <input type="file" id="file" name="file">
-            
-                    <center> <input type="submit" value="Simpan"> </center>
-                </form>                
-            </div>                
+                    <div style="margin-top: 10px;">
+                      <label for="file" style="margin-right: 10px;">Pilih File : </label>
+                      <input type="file" id="file" name="file" style="font-size: 18px;">
+                    </div>
+
+<center>
+    <input type="submit" value="Simpan">
+</center>
+
+
+                </form>
+            </div>
 
                     <!-- Sisipkan elemen-elemen formulir lainnya sesuai kebutuhan -->
                 </form>
